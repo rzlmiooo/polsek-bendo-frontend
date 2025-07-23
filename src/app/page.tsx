e26877b2b909bc2e11ccb0d2f3fc7cec94061461
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-07-23 11:21:14 +0700

    update SEO header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,22 @@ import { Analytics } from "@vercel/analytics/next";
 import Home from "./suspense/beranda/main-suspense";
 import Script from 'next/script';
 import { Metadata } from 'next';
+import Head from "next/head";
 
-export const metadata: Metadata = {
-  other: {
-    'google-site-verification': 'kPptZ5boJ9QHlGWamNOX1TXLEPZruxtoGj-WHwaT-JI',
-  },
-};
 
 export default function MainPage() {
   return (
     <Suspense fallback={<div>Redirect Home Page...</div>}>
+      <Head>
+        <head>
+          <title>Polsek Bendo</title>
+          <meta name="google-site-verification" content="kPptZ5boJ9QHlGWamNOX1TXLEPZruxtoGj-WHwaT-JI" />        
+        </head>
+      </Head>
       <Analytics />
       {/* <PathWrapper /> */}
-        <Home/>
-        <Script src="/node_modules/preline/preline.js" strategy="afterInteractive" />
+      <Home />
+      <Script src="/node_modules/preline/preline.js" strategy="afterInteractive" />
     </Suspense>
   );
 }
