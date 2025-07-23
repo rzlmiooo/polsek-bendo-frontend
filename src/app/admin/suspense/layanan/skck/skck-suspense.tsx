b5ae7892d574ca918a72014128264dcc64572803
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-07-23 09:28:51 +0700

    wider leftbar

--- src/app/admin/suspense/layanan/skck/skck-suspense.tsx
+++ src/app/admin/suspense/layanan/skck/skck-suspense.tsx
@@ -151,8 +151,8 @@ export default function KelolaSkck() {
             <header className="fixed top-0 left-0 w-full z-50">
                 <AdminNavbar />
             </header>
-            <section className="pt-20 bg-white py-24 antialiased dark:bg-gray-900 md:py-18 **lg:ps-65**">
-                <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+              <section className="pt-20 bg-white py-24 antialiased dark:bg-gray-900 md:py-18 **lg:ps-65**">
+                <div className="mx-auto max-w-screen-xl px-4 2xl:px-0 **lg:ml-[260px]**">
                     <div className="mx-auto max-w-5xl">
                         <div className="gap-4 sm:flex sm:items-center sm:justify-between">
                             <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">List Pemohon SKCK</h2>
