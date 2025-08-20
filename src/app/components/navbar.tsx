c017ca007c9fe67c4992a940a7c14e97a073c8ac
rzlmiooo <zrrizall10@example.com>
2025-08-20 20:44:57 +0700

    update Order page, update notification hovering & links, fix order's notification filtering

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,19 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const [token, setToken] = useState("")
   const hideTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+      if (typeof window !== 'undefined') {
+          const storedToken = localStorage.getItem('token');
+          if (storedToken) {
+              setToken(storedToken);
+          }
+      }
+  }, []);
 
   const showDropdown = () => {
     clearTimeout(hideTimeout.current!);
@@ -12,6 +22,19 @@ export default function Navbar() {
 
   const scheduleHide = () => {
     hideTimeout.current = setTimeout(() => setIsNavbarOpen(false), 300); // delay biar gak ke-close langsung
+  };
+
+  // profil
+  const [isProfilOpen, setIsProfilOpen] = useState(false);
+  const hideProfilTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const showProfilDropdown = () => {
+    clearTimeout(hideProfilTimeout.current!);
+    setIsProfilOpen(true);
+  };
+
+  const scheduleProfilHide = () => {
+    hideProfilTimeout.current = setTimeout(() => setIsProfilOpen(false), 200); // delay biar gak ke-close langsung
   };
 
   return (
@@ -84,7 +107,10 @@ export default function Navbar() {
 
               {/* Profil dropdown - mobile behavior optional */}
               <li>
-                <a href="/profil/struktur" className="block py-2 px-3 hover:text-[#996515]">
+                <button onClick={() => setIsProfilOpen(!isProfilOpen)} className="hidden py-2 px-2 md:block hover:text-[#996515]" onMouseEnter={showProfilDropdown} onMouseLeave={scheduleProfilHide}>
+                  Profil
+                </button>
+                <a href="/layanan" className="block md:hidden py-2 px-3 hover:text-[#996515]">
                   Profil
                 </a>
               </li>
@@ -100,6 +126,7 @@ export default function Navbar() {
 
             {/* Order Icon */}
             <div className="flex justify-center mt-4 md:mt-0">
+              {!token && (
               <a href="/login" className="flex justify-center gap-2 items-center py-2 px-6 hover:text-[#996515] rounded border-l-1 border-black">
                 <h2 className="font-bold">Login</h2>
                 <svg
@@ -114,6 +141,23 @@ export default function Navbar() {
                   />
                 </svg>
               </a>
+              )}
+              {token && (
+              <a href="/order" className="flex justify-center gap-2 items-center py-2 px-6 hover:text-[#996515] rounded border-l-1 border-black">
+                <h2 className="font-bold">Login</h2>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#505050"
+                    d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m-4-4v-3.075l5.525-5.5q.225-.225.5-.325t.55-.1q.3 0 .575.113t.5.337l.925.925q.2.225.313.5t.112.55t-.1.563t-.325.512l-5.5 5.5zm7.5-6.575l-.925-.925zm-6 5.075h.95l3.025-3.05l-.45-.475l-.475-.45L9.5 13.55zm3.525-3.525l-.475-.45l.925.925z"
+                  />
+                </svg>
+              </a>
+              )}
             </div>
           </div>
         </div>
@@ -127,6 +171,26 @@ export default function Navbar() {
             { href: "./layanan/pengaduan", icon: "icon-pengaduan", label: "Pengaduan Masyarakat" },
             { href: "./layanan/rencana", icon: "icon-rencana", label: "Rencana Acara" },
             { href: "./layanan/izin_keramaian", icon: "icon-izin", label: "Izin Keramaian" },
+          ].map((layanan, idx) => (
+            <a
+              key={idx}
+              href={layanan.href}
+              className="flex items-center gap-3 hover:text-yellow-700 transition-colors"
+            >
+              <div className="w-12 h-12 flex items-center justify-center border border-gray-300 rounded-sm">
+                <img src={`/icons/${layanan.icon}.svg`} alt={layanan.label} className="w-6 h-6" />
+              </div>
+              <span className="text-base font-semibold text-gray-700">{layanan.label}</span>
+            </a>
+          ))}
+        </div>
+      </section>
+      <section className={`fixed flex justify-center z-50 top-20 py-4 w-full bg-white shadow-lg transition-all duration-200 ${isProfilOpen ? 'opacity-100 pointer-events-auto visible' : 'opacity-0 pointer-events-none invisible'}`} onMouseEnter={showProfilDropdown} onMouseLeave={scheduleProfilHide}>
+        <div className="flex flex-wrap justify-center gap-8">
+          {[
+            { href: "./profil/struktur", icon: "icon-struktur", label: "Struktur Organisasi" },
+            { href: "./profil/forkopimca", icon: "icon-forkopimca", label: "FORKOPIMCA" },
+            { href: "./profil/biografi", icon: "icon-biografi", label: "Biografi" },
           ].map((layanan, idx) => (
             <a
               key={idx}
--- src/app/suspense/order/pm/order-pm-suspense.tsx
+++ src/app/suspense/order/pm/order-pm-suspense.tsx
@@ -1,16 +1,13 @@
-
 "use client";
 
 import axios from "axios";
-import AdminNavbar from "@/app/components/adminnavbar";
-import Footer from "../../../components/footer";
-import Navbar from "../../../components/navbar";
 import { useEffect, useState } from "react";
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from "next/navigation";
 import Link from 'next/link';
 import getUserId from '../../../utils/auth/page';
 import Head from "next/head";
+import formatTanggalIndonesia from "@/app/components/formatTanggal";
 
 interface PmDetail {
     id:string;
@@ -91,7 +88,7 @@ export default function OrderPm() {
             </Head>
         <div>
             <div className="sm:flex sm:justify-left">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-yellow-500 sm:text-2x1 m-4">Pengaduan Masyarakat</h3>
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-yellow-500 sm:text-2xl m-4">Pengaduan Masyarakat</h3>
             </div>     
             {pmData.map((pm, index) => (
                 <div key={index} className="grid grid-cols-2 md:grid-cols-6 lg:grid-cols-7 gap-y-8 py-10 border-b border-gray-700">
@@ -104,14 +101,14 @@ export default function OrderPm() {
 
                     <dl className="col-span-1">
                         <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Tanggal Pembuatan:</dt>
-                        <dd className="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{new Date(pm.complaint_date).toDateString()}</dd>
+                        <dd className="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{formatTanggalIndonesia(new Date(pm.complaint_date).toDateString())}</dd>
 
                     </dl>
 
                     {pm.complaint_status === "diterima" && (
                         <dl className="col-span-1">
                             <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Status:</dt>
-                            <dd className="me-2 mt-1.5 inline-flex items-center rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300">
+                            <dd className="me-2 mt-1.5 inline-flex items-center rounded bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
                                 <svg className="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.5 4h-13m13 16h-13M8 20v-3.333a2 2 0 0 1 .4-1.2L10 12.6a1 1 0 0 0 0-1.2L8.4 8.533a2 2 0 0 1-.4-1.2V4h8v3.333a2 2 0 0 1-.4 1.2L13.957 11.4a1 1 0 0 0 0 1.2l1.643 2.867a2 2 0 0 1 .4 1.2V20H8Z" />
                                 </svg>
@@ -145,28 +142,28 @@ export default function OrderPm() {
                     )}
 
                     <dl className="col-span-1">
-                        <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Title:</dt>
+                        <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Judul:</dt>
                         <dd className="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">
                             <a href="#" className="hover:underline">{pm.complaint_title}</a>
                         </dd>
                     </dl>
 
                     <dl className="col-span-1">
-                        <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Isi:</dt>
+                        <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Kategori:</dt>
+                        <dd className="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">
+                            <a href="#" className="hover:underline">{pm.complaint_category}</a>
+                        </dd>
+                    </dl>
+
+                    <dl className="col-span-1">
+                        <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Deskripsi:</dt>
                         <dd className="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">
                             <a href="#" className="hover:underline">{pm.complaint_content}</a>
                         </dd>
                     </dl>
 
-                    <dl className="col-span-1">
-                        <dt className="text-base font-medium text-gray-500 dark:text-gray-400">Keperluan:</dt>
-                        <dd className="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">
-                            <a href="#" className="hover:underline">{pm.contact}</a>
-                        </dd>
-                    </dl>
-
                     <div className="w-full grid sm:grid-cols-1 lg:flex lg:w-36 lg:items-center lg:justify-end gap-4">
-                        <Link href={`/order/pm/edit-pm?pm_id=${pm.id}?complainant_name=${pm.complainant_name}`} legacyBehavior>
+                        <Link href={`/order/pm/edit-pm?pm_id=${pm.id}?complainant_name=${pm.complainant_name}`}>
                             <button type="button" className="w-full rounded-lg border border-blue-700 px-3 py-2 text-center text-sm font-medium text-blue-700 hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-900 lg:w-auto">Edit</button>
                         </Link>
                     </div>
